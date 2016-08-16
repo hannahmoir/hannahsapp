7aca106d2db603bde28c850e5f6974a8ef0a5030
hannahmoir <hmoir.music@gmail.com>
2016-08-16 16:58:00 -0400

    5.2 revision

--- nameofapp/app/controllers/products_controller.rb
+++ nameofapp/app/controllers/products_controller.rb
@@ -6,7 +6,11 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
       search_term = params[:q]
-      @products = Product.where("name LIKE ?", "%#{search_term}%") 
+      if Rails.env.development?
+        @products = Product.where("name LIKE ?", "%#{search_term}%")
+      else
+        @products = Product.where("name iLIKE ?", "%#{search_term}%")
+      end
       # return our filterd list here
     else
       @products = Product.all
