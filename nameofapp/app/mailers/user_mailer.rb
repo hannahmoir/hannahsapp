c41953553a8348abd12928de2a9795a005518f4f
hannahmoir <hmoir.music@gmail.com>
2016-08-16 17:15:56 -0400

    5.3 mailers revision

--- nameofapp/app/mailers/user_mailer.rb
+++ nameofapp/app/mailers/user_mailer.rb
@@ -8,10 +8,4 @@ class UserMailer < ActionMailer::Base
         :subject => "A new contact form message from #{name}")
   end
   
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver
-  end
 end
